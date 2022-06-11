5a20699cd41ab0c92bdbdb7dbb0f9f81743c9054
Shabex <shabsibrah@gmail.com>
2022-06-11 12:55:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching (drivers, string){
+    return drivers.filter( (driver)=>driver.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch (drivers, newContent){
+    return drivers.filter(
+        (match)=> match.toLowerCase().indexOf(newContent.toLowerCase())===0);
+}
+
+function matchName(drivers, copyDriversName){
+    return drivers.filter((record)=>record.name===copyDriversName);
+}
